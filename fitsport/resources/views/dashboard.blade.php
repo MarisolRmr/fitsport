becad661f31d75a19f2c6481d0f00db4d0763218
“MarisolRmr” <2030112@upv.edu.mx>
2023-07-06 05:55:29 -0600

    vista dashboard admin, update vendor

---
+++ fitsport/resources/views/dashboard.blade.php
@@ -0,0 +1,143 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Administrador Dashboard
+@endsection
+
+@section('contenido_top')
+<div class="absolute bg-y-50 w-full top-0 bg-[url('{{asset ('img/admin.png')}}')] min-h-75">
+  <span class="absolute top-0 left-0 w-full h-full bg-black opacity-60"></span>
+</div>
+@endsection
+
+@section('contenido')
+
+<!-- cards -->
+<div class="w-full px-6 py-6 mx-auto">
+  <!-- cards row 2 -->
+  <div class="flex flex-wrap -mx-3">
+    <div class="w-full h-1/2 max-w-full px-3 mt-0 lg:w-6/12 lg:flex-none">
+      <img src="{{asset ('img/admin/admin_dash.png')}}" class="h-full w-full" alt="admin_dashboard" />   
+    </div>
+  </div>
+
+  <!-- row 1 -->
+  <div class="flex flex-wrap py-6 -mx-3">
+    <!-- card1 -->
+    <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+      <div class="relative flex flex-col min-w-0 break-words bg-white shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border" style="background-color: rgba(53, 58, 80, 0.67);">
+        <div class="flex-auto p-4">
+          <div class="flex flex-row -mx-3">
+            <div class="flex-none w-2/3 max-w-full px-3">
+              <div>
+                <p class="mb-0 font-sans text-sm font-semibold leading-normal uppercase text-white dark:opacity-60">Registrar</p>
+                <h5 class="mb-2 font-bold text-white">Gyms</h5>
+                <p class="mb-0 dark:text-white dark:opacity-60">
+                  <span class="text-sm font-bold leading-normal text-emerald-500">Gyms and Boxes</span>
+                </p>
+              </div>
+            </div>
+            <div class="px-3 text-right basis-1/3">
+              <div class="inline-block w-12 h-12 text-center rounded-circle bg-gradient-to-tl from-blue-500 to-violet-500">
+                <i class="ni leading-none ni-money-coins text-lg relative top-3.5 text-white"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- card2 -->
+    <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+      <div class="relative flex flex-col min-w-0 break-words bg-white shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border" style="background-color: rgba(53, 58, 80, 0.67);">
+        <div class="flex-auto p-4">
+          <div class="flex flex-row -mx-3">
+            <div class="flex-none w-2/3 max-w-full px-3">
+              <div>
+                <p class="mb-0 font-sans text-sm font-semibold leading-normal uppercase text-white dark:opacity-60">Registrar</p>
+                <h5 class="mb-2 font-bold text-white">Nutriólogos</h5>
+                <p class="mb-0 dark:text-white dark:opacity-60">
+                  <span class="text-sm font-bold leading-normal text-emerald-500">Nutriólogos</span>
+                </p>
+              </div>
+            </div>
+            <div class="px-3 text-right basis-1/3">
+              <div class="inline-block w-12 h-12 text-center rounded-circle bg-gradient-to-tl from-red-600 to-orange-600">
+                <i class="ni leading-none ni-world text-lg relative top-3.5 text-white"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- card3 -->
+    <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+      <div class="relative flex flex-col min-w-0 break-words bg-white shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border" style="background-color: rgba(53, 58, 80, 0.67);">
+        <div class="flex-auto p-4">
+          <div class="flex flex-row -mx-3">
+            <div class="flex-none w-2/3 max-w-full px-3">
+              <div>
+                <p class="mb-0 font-sans text-sm font-semibold leading-normal uppercase text-white dark:opacity-60">Registrar</p>
+                <h5 class="mb-2 font-bold text-white">Entrenadores</h5>
+                <p class="mb-0 dark:text-white dark:opacity-60">
+                  <span class="text-sm font-bold leading-normal text-emerald-500">Entrenadores</span>
+                </p>
+              </div>
+            </div>
+            <div class="px-3 text-right basis-1/3">
+              <div class="inline-block w-12 h-12 text-center rounded-circle bg-gradient-to-tl from-emerald-500 to-teal-400">
+                <i class="ni leading-none ni-paper-diploma text-lg relative top-3.5 text-white"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- card4 -->
+    <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+      <div class="relative flex flex-col min-w-0 break-words bg-white shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border" style="background-color: rgba(53, 58, 80, 0.67);">
+        <div class="flex-auto p-4">
+          <div class="flex flex-row -mx-3">
+            <div class="flex-none w-2/3 max-w-full px-3">
+              <div>
+                <p class="mb-0 font-sans text-sm font-semibold leading-normal uppercase text-white dark:opacity-60">Registrar</p>
+                <h5 class="mb-2 font-bold text-white">Atletas</h5>
+                <p class="mb-0 dark:text-white dark:opacity-60">
+                  <span class="text-sm font-bold leading-normal text-emerald-500">Atletas</span>
+                </p>
+              </div>
+            </div>
+            <div class="px-3 text-right basis-1/3">
+              <div class="inline-block w-12 h-12 text-center rounded-circle bg-gradient-to-tl from-emerald-500 to-teal-400">
+                <i class="ni leading-none ni-paper-diploma text-lg relative top-3.5 text-white"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <footer class="pt-4">
+    <div class="w-full px-6 mx-auto">
+      <div class="flex flex-wrap items-center -mx-3 lg:justify-between">
+        <div class="w-full max-w-full px-3 mt-0 mb-6 shrink-0 lg:mb-0 lg:w-1/2 lg:flex-none">
+          <div class="text-sm leading-normal text-center text-white lg:text-left">
+            ©
+            <script>
+              document.write(new Date().getFullYear() + ",");
+            </script>
+            made with <i class="fa fa-heart"></i> by Lorena Marisol Romero Hernández
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</div>
+<!-- end cards -->
+
+
+@endsection
---
+++ fitsport/resources/views/layouts/app.blade.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="#" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+
+    
+    {{-- Estilos de tailwind --}}
+    @vite('resources/css/app.css')
+
+    {{-- Scripts de tailwind --}}
+    @vite('resources/js/app.js')
+
+    <!-- Fonts and icons -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+
+
+    @vite('resources/css/nucleo-icons.css')
+    @vite('resources/css/nucleo-svg.css')
+    @vite('resources/css/argon-dashboard-tailwind.css')
+
+    <!-- Popper -->
+    <script src="https://unpkg.com/@popperjs/core@2"></script>
+
+    @vite('resources/js/plugins/chartjs.min.js')
+    @vite('resources/js/plugins/perfect-scrollbar.min.js')
+    @vite('resources/js/argon-dashboard-tailwind.js')
+    @vite('resources/js/argon-dashboard-tailwind.min.js')
+
+    <title>FitSport</title>
+</head>
+
+<body class="m-0 font-sans text-base antialiased font-normal dark:bg-slate-900 leading-default bg-gray-50 text-slate-500">
+  
+    {{-- @yield('contenido_top') --}}
+ 
+    <!-- sidenav  -->
+    <aside class="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-black border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0" aria-expanded="false" style="background-color: rgba(53, 58, 80, 0.67);">
+      <div class="h-19 text-center">
+        <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap dark:text-white text-slate-700" href="#">
+          <img src="{{asset ('img/logo.svg')}}" class="inline h-full max-w-full transition-all duration-200 dark:hidden ease-nav-brand max-h-8" alt="main_logo" />
+        </a>
+      </div>
+
+      <hr class="h-px mt-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent" />
+
+      <div class="items-center block w-auto max-h-screen overflow-auto h-sidenav grow basis-full">
+        <ul class="flex flex-col pl-0 mb-0">
+
+          <li class="mt-0.5 w-full">
+            <a class="py-2.7 text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="#">
+              <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                <i class="relative top-0 text-sm leading-normal text-blue-500 ni ni-tv-2"></i>
+              </div>
+              <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Dashboard</span>
+            </a>
+          </li>
+
+          {{-- <li class="w-full mt-4">
+            <h6 class="pl-6 ml-2 text-xs font-bold leading-tight uppercase text-white opacity-60">Registrar</h6>
+          </li> --}}
+
+          <li class="mt-0.5 w-full">
+            <a class="text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="#">
+              <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                <i class="relative top-0 text-sm leading-normal text-orange-500 ni ni-calendar-grid-58"></i>
+              </div>
+              <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Gyms and Boxes</span>
+            </a>
+          </li>
+
+          <li class="mt-0.5 w-full">
+            <a class="text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="#">
+              <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center fill-current stroke-0 text-center xl:p-2.5">
+                <i class="relative top-0 text-sm leading-normal text-emerald-500 ni ni-credit-card"></i>
+              </div>
+              <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Nutriólogos</span>
+            </a>
+          </li>
+
+          <li class="mt-0.5 w-full">
+            <a class="text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="#">
+              <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                <i class="relative top-0 text-sm leading-normal text-cyan-500 ni ni-app"></i>
+              </div>
+              <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Entrenadores</span>
+            </a>
+          </li>
+
+          <li class="mt-0.5 w-full">
+            <a class="text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="#">
+              <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                <i class="relative top-0 text-sm leading-normal text-red-600 ni ni-world-2"></i>
+              </div>
+              <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Atletas</span>
+            </a>
+          </li>
+
+          <li class="mt-0.5 w-full">
+            <a class="text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="#">
+              <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                <i class="relative top-0 text-sm leading-normal text-slate-700 ni ni-single-02"></i>
+              </div>
+              <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Ejercítate</span>
+            </a>
+          </li>
+
+        </ul>
+      </div>
+
+    </aside>
+
+
+    <!-- end sidenav -->
+
+
+     <!-- FONDO INTERNET -->
+    {{-- <div class="absolute w-full h-full top-0 bg-cover bg-center" style="background-image: url('https://cdn.wallpapersafari.com/3/96/eFMyB8.png'); min-height: 75px;">
+      <span class="absolute top-0 left-0 w-full h-full bg-black opacity-70"></span>
+    </div> --}}
+
+    <div class="absolute w-full h-full top-0 bg-cover bg-center" style="background-image: url('{{asset ('img/admin/fondo4.png')}}'); min-height: 75px;">
+      <span class="absolute top-0 left-0 w-full h-full bg-black opacity-70"></span>
+    </div>
+    
+  
+    <main class="relative h-full max-h-screen transition-all duration-200 ease-in-out xl:ml-68 rounded-xl">
+
+        <!-- Navbar -->
+      <nav class="relative flex flex-wrap items-center justify-between px-0 py-3 mx-6 transition-all ease-in shadow-none duration-250 rounded-2xl lg:flex-nowrap lg:justify-start" navbar-main navbar-scroll="false">
+        <div class="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit">
+          <nav>
+            <!-- breadcrumb -->
+            <ol class="flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16">
+              <li class="text-sm leading-normal">
+
+                <a class="text-white opacity-50" href="#"> FitSport</a>
+
+                
+              </li>
+              <li class="text-sm pl-2 capitalize leading-normal text-white before:float-left before:pr-2 before:text-white before:content-['/']" aria-current="page">@yield('titulo')</li>
+            </ol>
+            <h4 class="mb-0 font-bold text-white capitalize"> @yield('titulo') </h4>
+          </nav>
+
+          <div class="flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto">
+            <div class="flex items-center md:ml-auto md:pr-4">
+              <div class="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease">
+               
+                
+              </div>
+            </div>
+            <ul class="flex flex-row justify-end pl-0 mb-0 list-none md-max:w-full">
+
+              <li class="flex items-center">
+                <a href="#" class="block px-0 py-2 text-sm font-semibold text-white transition-all ease-nav-brand">
+                  <i class="fa fa-user sm:mr-1"></i>
+                  <span class="hidden sm:inline p-2"> Username </span>
+                </a>
+              </li>
+
+              <li class="flex items-center">
+                <a href="#" class="block px-0 py-2 text-sm font-semibold text-white transition-all ease-nav-brand">
+                  <span class="hidden sm:inline"> Cerrar Sesión </span> 
+                </a>
+              </li>
+
+
+
+              {{-- <li class="flex items-center">
+                <a href="#" class="block px-0 py-2 text-sm font-semibold text-white transition-all ease-nav-brand">
+                  <i class="fa fa-user sm:mr-1"></i>
+                  <span class="hidden sm:inline"> Iniciar Sesión</span>
+                </a>
+              </li> --}}
+
+             
+              <li class="flex items-center pl-4 xl:hidden">
+                <a href="javascript:;" class="block p-0 text-sm text-white transition-all ease-nav-brand" sidenav-trigger>
+                  <div class="w-4.5 overflow-hidden">
+                    <i class="ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all"></i>
+                    <i class="ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all"></i>
+                    <i class="ease relative block h-0.5 rounded-sm bg-white transition-all"></i>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!-- end Navbar -->
+
+      @yield('contenido')
+
+
+    </main>
+
+   
+
+  </body>
+
+</html>
